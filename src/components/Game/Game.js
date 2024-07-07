12db7b94566b4b333a873a10eef09fc540feec99
rimonaldo <rimons2804@gmail.com>
2024-07-07 09:37:28 -0400

    guess input state flow from parent cmp

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput";
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guessInput, setGuessInput] = React.useState("");
+
+  
+
+  const handleSubmitGuess = () => {
+    // if(guessInput.length < 5){
+    //   return
+    // }
+    const formatedGuess = guessInput.toLocaleUpperCase("en-US");
+    console.log("GUESS:", formatedGuess);
+  };
+
+  return (
+    <>
+      <GuessInput
+        input={guessInput}
+        setInput={setGuessInput}
+        submitGuess={handleSubmitGuess}
+      />
+    </>
+  );
 }
 
 export default Game;
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function GuessInput() {
-  return <div></div>;
+function GuessInput({ input, setInput, submitGuess }) {
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(ev) => {
+        ev.preventDefault();
+        submitGuess();
+        setInput("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter Guess:</label>
+      <input
+        value={input}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(ev) => {
+          const guess = ev.target.value;
+          if (guess.length - 1 === 5) {
+            return;
+          } else {
+            setInput(ev.target.value.toUpperCase());
+          }
+        }}
+      ></input>
+    </form>
+  );
 }
 
 export default GuessInput;

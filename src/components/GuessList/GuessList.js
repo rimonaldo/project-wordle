89ae87ea33f660ac8a59ee1e61a9e6cfe95bf600
rimonaldo <rimons2804@gmail.com>
2024-07-07 10:11:21 -0400

    added GuessList cmp for EX-02

---
+++ src/components/GuessList/GuessList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GuessList() {
+  return (
+    <div className="guess-results">
+      <div className="guess">banana</div>
+    </div>
+  );
+}
+
+export default GuessList;
